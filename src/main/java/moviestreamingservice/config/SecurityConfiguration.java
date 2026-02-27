077d61ac24ca3e985c54a26c3c34bc32158f1ac1
anastasristeski <anastas.risteski5@gmail.com>
2026-02-27 03:19:52 +0100

    env

--- src/main/java/moviestreamingservice/config/SecurityConfiguration.java
+++ src/main/java/moviestreamingservice/config/SecurityConfiguration.java
@@ -66,7 +66,7 @@ public class SecurityConfiguration {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("http://localhost:3000",System.getenv("FRONTEND_URL")));
         configuration.setAllowedMethods(List.of("GET","POST","PUT","DELETE"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
